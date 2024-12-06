a5e85de6c453a78c18c9b31c66dccde9efc4d064
Karolill <karoline.l.langli@icloud.com>
2024-12-06 17:07:08 +0100

    Revert "Add sounds"
    This reverts commit ad36878ab1f6a8b752a4d05bbcd4d262b0ac99d4.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const teamSounds: Record<number, string> = {
-  1: "/sounds/cow.mp3", // Path for Team 1's sound
-  2: "/sounds/team2.mp3", // Path for Team 2's sound
-};
